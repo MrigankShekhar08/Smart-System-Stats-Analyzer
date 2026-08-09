cfc2fd1424b7943af385c7fa9f2153d9657ef447
Mridul Pathak <mridulpathak9473@gmail.com>
2026-08-09 09:48:35 +0530

    Version 2.0 of the python script

--- Scripts/ingest.py
+++ Scripts/ingest.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('/home/MridulPathak/Project1/Metrics/Container2.csv', mode='r', newline='') as file:
+with open('/home/Mridul/Smart-System-Stats-Analyzer/Metrics/Container2.csv', mode='r', newline='') as file:
     csv_reader = csv.reader(file)
     for row in csv_reader:
         print(row)
