77793a5320b6efb5cbd5af833dbaea1d7ead97e0
Mridul Pathak <mridulpathak9473@gmail.com>
2026-08-09 10:59:58 +0530

    Started using relative paths

--- Scripts/ingest.py
+++ Scripts/ingest.py
@@ -1,6 +1,8 @@
+from pathlib import Path
 import csv
-
-with open('/home/Mridul/Smart-System-Stats-Analyzer/Metrics/Container2.csv', mode='r', newline='') as file:
+script_p = Path(__file__).parent
+script_r = script_p.parent
+with open(script_r/"Metrics"/"Container2.csv", mode='r', newline='') as file:
     csv_reader = csv.reader(file)
     for row in csv_reader:
         print(row)
